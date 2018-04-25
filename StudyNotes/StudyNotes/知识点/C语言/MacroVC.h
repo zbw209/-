aa735736311e921b8ec0f9f28770c39373773959
zhoubiwen <564648237@qq.com>
2018-04-25 09:37:19 +0800

    添加多参数变量处理方案

---
+++ StudyNotes/StudyNotes/知识点/C语言/MacroVC.h
@@ -0,0 +1,13 @@
+//
+//  MacroVC.h
+//  StudyNotes
+//
+//  Created by zhoubiwen on 2018/4/24.
+//  Copyright © 2018年 zhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MacroVC : UIViewController
+
+@end
